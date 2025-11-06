90fba411e3c5b657905143cc2c379d449b5b040a
L4ish <laish_rodrigues@hotmail.com>
2025-11-06 16:53:10 -0300

    navbar e footer melhorados

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,26 +1,52 @@
-import { EnvelopeIcon, GithubLogoIcon } from "@phosphor-icons/react"
+import { EnvelopeIcon, GithubLogoIcon } from "@phosphor-icons/react";
 
 function Footer() {
-    const data = new Date().getFullYear()
+  const data = new Date().getFullYear();
 
-    return (
-        <>
-            <div className="flex justify-center bg-lime-900 text-white">
-                <div className="container flex flex-col items-center py-4">
-                    <p className="text-xl font-bold">
-                        Commit8 | Copyright {data}
-                    </p>
-                    <p className="text-lg">Contato: </p>
-                    <div className="flex gap-2">
-                        <GithubLogoIcon size={38} weight="duotone" />
-                        <EnvelopeIcon size={38} weight="duotone" />
-                    </div>
+  return (
+    <>
+      <div className="flex bg-[linear-gradient(135deg,#0D0D0D_0%,#76A687_40%,#D97652_100%)] text-white">
+        <div className="w-full flex text-xl justify-between items-center py-1.5 font-yusei">
+          <div className="flex items-center">
+            <img
+              src="https://i.imgur.com/NKTrcQT.png"
+              alt="logo"
+              className="w-16 transition-transform duration-300 hover:scale-110 hover:translate-x"
+            />
+            <span>Commit8 | Copyright {data}</span>
+          </div>
 
-                </div>
+          <div className="flex gap-5 px-2">
+            <p>Venha nos conhecer</p>
 
-            </div>
-        </>
-    )
-} 
+            <a
+              href="https://github.com/Commit8"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <GithubLogoIcon
+                className="transition-transform duration-300 hover:scale-125 hover:translate-x"
+                size={38}
+                weight="light"
+              />
+            </a>
 
-export default Footer
+            <a
+              href="mailto:devteam01.js09@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <EnvelopeIcon
+                className="transition-transform duration-300 hover:scale-125 hover:translate-x"
+                size={38}
+                weight="light"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,14 +1,22 @@
 function Navbar() {
     return (
         <>
-            <div className="w-full flex justify-center py-4 bg-amber-500 text-white">
+            <div className="w-full flex bg-[linear-gradient(135deg,#4D5159_0%,#76A687_40%,#D97652_100%)] text-white font-yusei">
 
-                <div className="container flex justify-between text-lg mx-8"> Maria Gasolina
+                <div className="w-full flex items-center justify-between"> 
 
-                    <div className="flex gap-4">
-                        Home |  
-                        Corridas |
-                        Categorias
+                    <div className="flex items-center">
+                        <img src="https://i.imgur.com/n1PW3bR.png" alt="logo" className="w-25 transition-transform duration-300 hover:scale-125 hover:translate-x" />
+                        <span className="text-4xl">Maria Gasolina</span>  
+                    </div>
+                  
+
+                    <div className="flex gap-4 text-xl px-4">
+                       <span className="hover:text-[#0D0D0D]"> Home</span>
+                       <span>|</span>
+                       <span className="hover:text-[#0D0D0D]">Corridas</span>
+                       <span>|</span>
+                       <span className="hover:text-[#0D0D0D]"> Categorias</span>
                     </div>
 
                 </div>
