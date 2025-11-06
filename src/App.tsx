ac50bbcf354fa53a7b13530af82f555c50abdb28
Gabriella <gabriella.parra05@gmail.com>
2025-11-06 15:48:34 -0300

    imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,60 @@
-import type { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import DeletarCategoria from "./components/categoria/deletarcategoria/DeletarCategoria";
+import ListaCategorias from "./components/categoria/listacategorias/ListaCategorias";
+import CancelarCorrida from "./components/corrida/cancelarcorrida/CancelarCorrida";
+import FormCorrida from "./components/corrida/fomsCorrida/FormsCorrida";
+import ListaCorrida from "./components/corrida/listacorrida/ListaCorrida";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
 
 function App() {
     return (
         <>
-            <AuthProvider>
-                <ToastContainer />
-                <BrowserRouter>
-                    <Navbar />
-                    <div className="min-h-[80vh]">
-                        <Routes>
-                            <Route path="/" element={<Home />} />
-                            <Route path="/home" element={<Home />} />
-                            <Route path="/categoria" element={<Categoria />} />
-                            <Route path="/temas" element={<ListaTemas />} />
-                            <Route
-                                path="/cadastrartema"
-                                element={<FormTema />}
-                            />
-                            <Route
-                                path="/atualizarcorrida/:id"
-                                element={<FormCorrida />}
-                            />
+            <ToastContainer />
+            <BrowserRouter>
+                <Navbar />
+                <div className="min-h-[80vh]">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/home" element={<Home />} />
+                        <Route path="/categoria" element={<Categoria />} />
+                        <Route
+                            path="/Listacategorias"
+                            element={<ListaCategorias />}
+                        />
+                        <Route
+                            path="/cadastrarcategoria"
+                            element={<FormCategoria />}
+                        />
+                        <Route
+                            path="/atualizarcategoria"
+                            element={<FormCategoria />}
+                        />
+                        <Route
+                            path="/deletarcategoria/:id"
+                            element={<DeletarCategoria />}
+                        />
+                        <Route
+                            path="/atualizarcorrida/:id"
+                            element={<FormCorrida />}
+                        />
 
-                            <Route
-                                path="/corridas"
-                                element={<ListaCorrida />}
-                            />
-                            <Route
-                                path="/cadastrarcorrida"
-                                element={<FormCorrida />}
-                            />
+                        <Route path="/corridas" element={<ListaCorrida />} />
+                        <Route
+                            path="/cadastrarcorrida"
+                            element={<FormCorrida />}
+                        />
 
-                            <Route
-                                path="/cancelarcorrida/:id"
-                                element={<CancelarCorrida />}
-                            />
-                        </Routes>
-                    </div>
-                    <Footer />
-                </BrowserRouter>
-            </AuthProvider>
+                        <Route
+                            path="/cancelarcorrida/:id"
+                            element={<CancelarCorrida />}
+                        />
+                    </Routes>
+                </div>
+                <Footer />
+            </BrowserRouter>
         </>
     );
 }
