680dd1888afe7114fa891491d3f930b22247d8b8
L4ish <laish_rodrigues@hotmail.com>
2025-11-06 13:56:45 -0300

    estrutura da navbar e footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import Footer from "./components/footer/Footer"
+import Navbar from "./components/navbar/Navbar"
+
 function App() {
 
   return (
     <>
-    
+           <Navbar/>
+           <div className="min-h-[80vh]">
+
+
+           </div>
+           <Footer />
 
     </>
   )
