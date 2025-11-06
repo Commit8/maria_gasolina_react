912236fbc3d7e0df2e37808b576d6dcec9dc9cc1
DisturbedMoss <disturbedmoss35@gmail.com>
2025-11-06 11:17:13 -0300

    Corrigindo erros no App.tsx

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,14 @@
+
+const Home = () => {
+  return (
+    <>
+        <div className="bg-indigo-500">
+          <div className="container">
+            
+          </div>
+        </div>
+    </>
+  )
+}
+
+export default Home
