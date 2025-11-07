59761009253b8ca315a4ef1333a71913fe94d7b8
L4ish <laish_rodrigues@hotmail.com>
2025-11-07 13:21:55 -0300

    botão para fechar menu hamburguer

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -11,7 +11,7 @@ function Navbar() {
       <div className="w-full flex bg-[linear-gradient(135deg,#4D5159_0%,#76A687_40%,#D97652_100%)] text-white font-yusei md:px-4 lg:px-6">
 
         <div className="w-full flex items-center justify-between">
-
+          {/* garantir que a logo apareca ou não a depender do menu(true ou false) e sm:flex para fazer a logo aparecer mesmo com o menu true */}
           <Link
             to="/home"
             className={`flex items-center space-x-2 text-3xl font-extrabold text-white tracking-wide ${
@@ -22,7 +22,7 @@ function Navbar() {
           <div className="flex items-center">
             <img
               src="https://i.imgur.com/n1PW3bR.png"
-              alt="logo"
+              alt="logo" 
               className="border-[] w-25 transition-transform duration-300 hover:scale-125 hover:translate-x"
             />
             <a href="/home"className=" text-3xl md:text-4xl lg:text-5xl ">
@@ -32,6 +32,8 @@ function Navbar() {
           </Link>
 
         <div className="sm:hidden">
+          {/*Menu hamburguer, 'prev' é o valor anterior contido no IsNavOpen. Quando é clicado inverte o valor contido em isNavOpen = !prev.*/}
+          {/* if ternario para decidir o icone que aparecera */}
 
           <div
             className="cursor-pointer hover:text-white absolute top-0 right-0 px-6 py-6 z-11"
@@ -40,9 +42,16 @@ function Navbar() {
             {isNavOpen ? <XIcon size={32} /> : <ListIcon size={32} />}
           </div>
 
+          {/*if ternario, decide quando mostrar o menu dependendo se estiver clicado(true) ou não(false) */}
          <div className={isNavOpen ? "mostraMenuNav" : "escondeMenuNav"}>
+
+          {/* botão para fechar menu */}
+          <button onClick={() =>  setIsNavOpen(false)} className="cursor-pointer absolute top-4 right-6 text-white hover:text-[#0D0D0D]">
+              <XIcon size={32}/>
+          </button>
+
           <div className="flex gap-4 text-xl px-4">
-            <Link to="/home" className="hover:text-[#]" onClick={() => setIsNavOpen(false)}>
+            <Link to="/home" className="hover:text-[#0D0D0D]" onClick={() => setIsNavOpen(false)}>
               {" "}
               Home{" "}
             </Link>
@@ -84,7 +93,7 @@ function Navbar() {
           display: none;
         }
         .mostraMenuNav{
-          background: #4d5159;
+          background: #76A687;
           display: flex;
           position: absolute;
           top: 0;
