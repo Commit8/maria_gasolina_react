63933382a39e867cd6b2c68f05ec3a5728578509
DisturbedMoss <disturbedmoss35@gmail.com>
2025-11-06 21:59:58 -0300

    Corrigindo responsividade do corpo da HOME

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -9,27 +9,49 @@ const Home = () => {
   };
   return (
     <>
-      <div className="bg-indigo-500 flex flex-col justify-center items-center">
-        <h1 className="md:pt-30 font-bold text-4xl ">
-          Viaje com quem vai na mesma direção — simples, seguro e econômico.
-        </h1>
+      <div className="flex flex-col justify-center items-center bg-[#FAF9F6]">
+        <div className="relative w-full py-20 md:py-0 md:h-[550px] overflow-hidden">
+          <img
+            src="https://ik.imagekit.io/Disturbedmoss/joey-kyber-45FJgZMXCK8-unsplash.jpg?updatedAt=1762470445952"
+            alt="Vista de uma cidade movimentada"
+            className="absolute inset-0 w-full h-full object-cover"
+          />
 
-        <div className="sm:container bg-white mt-40 rounded-t-3xl md:pt-5">
-          <div className="w-full flex justify-center">
-            <button
-              className="w-40 cursor-pointer font-semibold bg-[#D97652] rounded py-2 hover:bg-[#d65b2e]"
-              onClick={handleCadastrarCorrida}
-            >
-              Cadastrar Corrida
-            </button>
+          <div className="absolute inset-0 z-10 bg-black/50"></div>
+
+          <div className="md:w-1/2 lg:w-3/5 flex flex-col gap-5 sm:items-center md:items-start">
+            <h1 className="sm:pt-15 ml-10 md:absolute font-bold text-3xl sm:text-6xl z-10 text-white text-shadow-md">
+              <p>Viaje com quem vai na mesma direção</p>simples, seguro e econômico.
+            </h1>
+
+            <div className='md:absolute ml-10 w-30 h-40 sm:top-85 md:top-85 lg:top-65 md:left-30 z-20'>
+              <button
+                className="w-40 cursor-pointer font-semibold bg-[#D97652] rounded py-2 hover:bg-[#d65b2e] z-20"
+                onClick={handleCadastrarCorrida}
+              >
+                Cadastrar Corrida
+              </button>
+            </div>
           </div>
 
-          <div className="w-full md:my-10 p-5 justify-center">
+          <div className="absolute bottom-0 left-0 w-full h-40 md:h-28 z-30">
+            <svg
+              viewBox="0 0 1000 100"
+              preserveAspectRatio="none"
+              className="w-full h-full fill-current text-gray-50"
+            >
+              <path d="M0,30 Q250,100 500,50 T1000,50 L1000,100 L0,100 Z" />
+            </svg>
+          </div>
+        </div>
 
-            <h2 className="text-4xl font-extrabold mb-12 text-center text-shadow-md">Como Funciona</h2>
+        <div className="sm:container bg-[#FAF9F6] rounded-t-3xl md:pt-5">
+          <div className="w-full md:my-10 p-5 justify-center">
+            <h2 className="text-4xl font-extrabold mb-12 text-center text-shadow-md">
+              Como Funciona
+            </h2>
 
             <div className="grid grid-cols-1 md:grid-cols-2 gap-2 justify-center">
-
               <div className="bg-white grid grid-cols-3 rounded-2xl w-2md border border-slate-400 shadow-md">
                 <div className="bg-[#97bda5] w-full flex justify-center items-center rounded-l-2xl">
                   <img
@@ -39,9 +61,12 @@ const Home = () => {
                   />
                 </div>
                 <div className="w-full col-span-2 pl-2 pt-2 gap-4">
-                  <h3 className="md:text-xl font-bold">Motoristas publicam seu destino</h3>
+                  <h3 className="md:text-xl font-bold">
+                    Motoristas publicam seu destino
+                  </h3>
                   <p className="text-[#3d3d3d]">
-                    Junte-se a mais de 500 mil motoristas cadastrados em todo Brasil.
+                    Junte-se a mais de 500 mil motoristas cadastrados em todo
+                    Brasil.
                   </p>
                 </div>
               </div>
@@ -95,11 +120,11 @@ const Home = () => {
                     Mobilidade que conecta pessoas e destinos
                   </h3>
                   <p className="text-[#3d3d3d]">
-                    Mais de 100 cidades conectadas todos os dias por meio de viagens compartilhadas.
+                    Mais de 100 cidades conectadas todos os dias por meio de
+                    viagens compartilhadas.
                   </p>
                 </div>
               </div>
-
             </div>
           </div>
 
@@ -149,7 +174,9 @@ const Home = () => {
           </div>
 
           <div className="p-5">
-            <h2 className="text-4xl font-extrabold mb-12 text-center text-shadow-md">Sobre Nós</h2>
+            <h2 className="text-4xl font-extrabold mb-12 text-center text-shadow-md">
+              Sobre Nós
+            </h2>
 
             <div className="flex flex-col justify-center">
               <p className=" gap-1 pb-8 text-2xl text-center">
@@ -162,7 +189,7 @@ const Home = () => {
                 soluções inovadoras, colaborativas e acessíveis.
               </p>
             </div>
-            
+
             <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
               <CardDevs
                 foto="https://i.postimg.cc/B68Yd9VQ/IMG-3370-1.png"
